e8b34d901b310bcffae656f323aa1c04c2bc5948
Ferid <feridamir187@gmail.com>
2024-06-08 22:49:50 +0400

    feat: syncing slider with the animation frame

--- src/components/visualizer/sorting/Container.tsx
+++ src/components/visualizer/sorting/Container.tsx
@@ -7,28 +7,35 @@ import {
 } from "solid-js";
 import BubbleService from "../../../services/sorting/bubble.service";
 import TimeLine from "../../common/timeline/TimeLine";
-import { TUniqueArr } from "../../../types";
+import { TResult, TUniqueArr } from "../../../types";
 
 const arr = [
   87, 352, 168, 427, 420, 419, 198, 155, 174, 138, 23, 102, 7, 247, 358, 394,
-  265, 151, 386, 329, 24, 55, 162, 295, 101, 398, 209, 161, 233, 215, 450, 133,
-  21, 121, 179, 69, 296, 66, 218, 335, 260, 331, 257, 111, 431, 42, 88, 383, 54,
-  245, 59, 229, 393, 359, 40, 171, 319, 26, 310, 321, 166, 374, 392, 377, 49,
-  327, 292, 342, 86, 409, 212, 286, 52, 31, 299, 223, 8, 181, 71, 110, 323, 262,
-  371, 122, 194, 395, 380, 158, 14, 185, 269, 287, 182, 202, 108, 416, 320, 127,
-  230, 340, 175, 130, 48, 124, 414, 346, 255, 30, 159, 100, 368, 213, 445, 38,
-  95, 17, 177, 57, 210, 242, 190, 109, 28, 326, 334, 443, 199, 136, 281, 446,
-  231, 217, 77, 303, 197, 267, 83, 339, 253, 259, 65, 426, 333, 205, 276, 97,
-  27, 311, 330, 92,
+  // 265, 151, 386, 329, 24, 55, 162, 295, 101, 398, 209, 161, 233, 215, 450, 133,
+  // 21, 121, 179, 69, 296, 66, 218, 335, 260, 331, 257, 111, 431, 42, 88, 383, 54,
+  // 245, 59, 229, 393, 359, 40, 171, 319, 26, 310, 321, 166, 374, 392, 377, 49,
+  // 327, 292, 342, 86, 409, 212, 286, 52, 31, 299, 223, 8, 181, 71, 110, 323, 262,
+  // 371, 122, 194, 395, 380, 158, 14, 185, 269, 287, 182, 202, 108, 416, 320, 127,
+  // 230, 340, 175, 130, 48, 124, 414, 346, 255, 30, 159, 100, 368, 213, 445, 38,
+  // 95, 17, 177, 57, 210, 242, 190, 109, 28, 326, 334, 443, 199, 136, 281, 446,
+  // 231, 217, 77, 303, 197, 267, 83, 339, 253, 259, 65, 426, 333, 205, 276, 97,
+  // 27, 311, 330, 92,
 ];
 
 const Container = () => {
   const [containerWidth, setContainerWidth] = createSignal(0);
   const [containerHeight, setContainerHeight] = createSignal(0);
+  const [data, setData] = createSignal<TResult[]>([]);
+  const [frameIdx, setFrameIdx] = createSignal(0);
+  const [isPaused, setIsPaused] = createSignal(true);
+  const [isDone, setIsDone] = createSignal(false);
   const [isAnimating, setIsAnimating] = createSignal(false);
 
+  const derivative = () => (frameIdx() / data().length || 0) * 100;
+
   let svg: SVGElement;
-  const data: number[][] = [];
+  let sliderRef!: HTMLDivElement;
+
   const uniqueArr: Accessor<TUniqueArr[]> = createMemo(() =>
     arr.map((itm, idx) => {
       return { value: itm, id: idx };
@@ -39,14 +46,15 @@ const Container = () => {
 
   createEffect(() => {
     svg.replaceChildren();
-    if (!isAnimating()) {
-      service.draw(containerWidth(), containerHeight(), svg);
+    if (!isAnimating() && isPaused()) {
+      service.draw(containerWidth(), containerHeight(), svg, frameIdx());
     }
   });
 
   onMount(() => {
     setContainerWidth(svg.clientWidth);
     setContainerHeight(svg.clientHeight);
+    setData(service.getData);
     service.draw(containerWidth(), containerHeight(), svg);
   });
 
@@ -55,21 +63,76 @@ const Container = () => {
     setContainerHeight(svg.clientHeight);
   });
 
-  const handleAnimate = async () => {
+  const handleStartAnimation = async (event: MouseEvent) => {
+    event.stopPropagation();
+    setIsPaused(false);
+    setIsDone(false);
     setIsAnimating(true);
-    await service.animate(containerWidth(), containerHeight(), svg);
+    for await (const val of service.animate(
+      containerWidth(),
+      containerHeight(),
+      svg,
+      isPaused,
+      frameIdx
+    )) {
+      setFrameIdx(val);
+    }
+    setIsPaused(true);
+    if (frameIdx() >= service.getData.length - 1) {
+      setIsDone(true);
+    }
+  };
+
+  const handleSliderClick = (event: MouseEvent) => {
+    event.stopPropagation();
+    if (!sliderRef) return;
+
+    const pageX = event.pageX;
+    const sliderLength = sliderRef.clientWidth;
+    const sliderToFrameLenght = sliderLength / data().length;
+    const leftPosition = sliderRef.getBoundingClientRect().left;
+    let positionClicked = pageX - leftPosition;
+
+    if (positionClicked > sliderLength) {
+      positionClicked = sliderLength;
+    }
+
+    if (positionClicked < 0) {
+      positionClicked = 0;
+    }
+
+    const positionClickIdx = Math.round(positionClicked / sliderToFrameLenght);
+    setFrameIdx(positionClickIdx);
+  };
+
+  const handlePauseAnimation = () => {
+    setIsPaused(true);
+    setIsAnimating(true);
+  };
+
+  const handleReplayAnimation = (event: MouseEvent) => {
+    setFrameIdx(0);
+    handleStartAnimation(event);
   };
 
   return (
     <div class="w-[100%] p-4 border border-solid min-h-[400px] min-w-[450px] border-red-500 best">
-      <div class="p-2 border relative h-[100%] max-h-[100%] border-red-500">
-        <svg
-          ref={(ele) => (svg = ele)}
-          class="w-[100%]  min-h-[400px]"
-          onclick={handleAnimate}
-        ></svg>
-        <div class="w-[85%] absolute bottom-4 left-[50%] translate-x-[-50%]">
-          <TimeLine data={data} />
+      <div class="p-5 border relative h-[100%] max-h-[100%] border-red-500">
+        <svg ref={(ele) => (svg = ele)} class="w-[100%]  min-h-[400px]"></svg>
+        <div class="w-[100%] absolute bottom-4 left-[50%] translate-x-[-50%] px-5">
+          <div class="w-[100%]">
+            <TimeLine
+              ref={sliderRef}
+              data={data}
+              isPaused={isPaused}
+              play={handleStartAnimation}
+              replay={handleReplayAnimation}
+              pause={handlePauseAnimation}
+              onSliderClick={handleSliderClick}
+              isDone={isDone}
+              derivative={derivative}
+            />
+          </div>
         </div>
       </div>
     </div>
--- src/services/sorting/bubble.service.ts
+++ src/services/sorting/bubble.service.ts
@@ -1,38 +1,45 @@
 import * as d3 from "d3";
 import BarsService from "./bars.service";
-import { TUniqueArr } from "../../types";
+import { TUniqueArr, TResult } from "../../types";
+import { Accessor } from "solid-js";
 
-type TResult = {
-  data: {
+type TBar = d3.Selection<
+  d3.BaseType | SVGRectElement,
+  {
     rank: number;
     value: number;
     compare: boolean;
     sorted: boolean;
     id: number;
-  }[];
-};
+  },
+  SVGGElement,
+  unknown
+>;
 
 class BubbleService extends BarsService {
   private data: TResult[];
   constructor(private arr: TUniqueArr[]) {
     super();
     this.data = this.sort();
-    console.log(this.data);
+  }
+
+  get getData() {
+    return this.data;
   }
 
   draw(
     containerWidth: number,
     containerHeight: number,
-    container: SVGElement
+    container: SVGElement,
+    frameIdx: number = 0
   ): void {
     const svg = d3.select(container);
-    const bar = svg.append("g").attr("fill", "steelblue").selectAll("rect");
     const data = this.data.slice();
     const x = this.scaleX(containerWidth, data[0].data);
     const y = this.scaleY(containerHeight, data[0].data);
-
+    const bar = svg.append("g").attr("fill", "steelblue").selectAll("rect");
     bar
-      .data(data[0].data, (d: any) => d.value)
+      .data(data[frameIdx].data, (d: any) => d.value)
       .join(
         (enter) =>
           enter
@@ -40,31 +47,46 @@ class BubbleService extends BarsService {
             .attr("x", (d) => x(d.rank) as number)
             .attr("y", (d) => y(d.value) as number)
             .attr("height", (d) => y(0) - y(d.value))
-            .attr("width", (_) => x.bandwidth()),
+            .attr("width", (_) => x.bandwidth())
+            .attr("fill", (d) =>
+              d.compare ? "red" : d.sorted ? "green" : "steelblue"
+            ),
         (update) => update,
         (exit) => exit
       );
   }
 
-  async animate(
+  //notes:
+  /**
+   * normal
+   * duration 100
+   * delay between animations 500
+   * waiting time 1000
+   */
+  async *animate(
     containerWidth: number,
     containerHeight: number,
-    container: SVGElement
+    container: SVGElement,
+    isPaused: Accessor<boolean>,
+    frameIdx: Accessor<number>
   ) {
     const svg = d3.select(container);
     const data = this.data.slice();
     const x = this.scaleX(containerWidth, data[0].data);
     const y = this.scaleY(containerHeight, data[0].data);
     svg.selectChildren().remove();
-    let bar = svg.append("g").attr("fill", "steelblue").selectAll("rect");
-    //notes:
-    /**
-     * normal
-     * duration 100
-     * delay between animations 500
-     * waiting time 1000
-     */
-    for (let i = 0; i < data.length; i++) {
+    let bar: TBar = svg.append("g").attr("fill", "steelblue").selectAll("rect");
+
+    let timer;
+
+    for (let i = frameIdx() || 0; i < data.length; i++) {
+      if (isPaused()) {
+        timer && clearTimeout(timer);
+        bar.interrupt();
+        svg.selectChildren().remove();
+        this.draw(containerWidth, containerHeight, container, i - 1);
+        return i - 1;
+      }
       bar = bar
         .data(data[i].data, (d: any) => d.id)
         .join(
@@ -74,26 +96,27 @@ class BubbleService extends BarsService {
               .attr("x", (d) => x(d.rank) as number)
               .attr("y", (d) => y(d.value) as number)
               .attr("height", (d) => y(0) - y(d.value))
+              .attr("fill", (d) =>
+                d.compare ? "red" : d.sorted ? "green" : "steelblue"
+              )
               .attr("width", (_) => x.bandwidth()),
           (update) =>
             update.call((update) =>
               update
                 .transition()
-                .duration(100)
+                .duration(10)
                 .ease(d3.easePolyInOut)
                 .attr("fill", (d) =>
                   d.compare ? "red" : d.sorted ? "green" : "steelblue"
                 )
-                .transition()
-                .delay(500)
-                .duration(100)
-                .ease(d3.easeLinear)
                 .attr("x", (d) => x(d.rank) as number)
             ),
           (exit) => exit
         );
-      await this.timer(1000);
+      timer = await this.timer(100);
+      yield i;
     }
+    return data.length - 1;
   }
 
   private sort(): TResult[] {
@@ -116,6 +139,20 @@ class BubbleService extends BarsService {
     for (let i = 0; i < arrCopy.length; i++) {
       const prevData = result[result.length - 1].data;
       for (let j = 0; j < arrCopy.length - i - 1; j++) {
+        const before = {
+          data: arrCopy.map((itm, idx) => {
+            return {
+              rank: idx,
+              value: itm.value,
+              id: itm.id,
+              sorted: prevData[idx].sorted,
+              compare:
+                ((idx === j || idx === j + 1) && j < arrCopy.length - 1 - i) ||
+                false,
+            };
+          }),
+        };
+        result.push(before);
         if (arrCopy[j].value > arrCopy[j + 1].value) {
           this.swap(arrCopy, j, j + 1);
         }
@@ -159,8 +196,12 @@ class BubbleService extends BarsService {
     arr[rht] = tmp;
   }
 
-  private timer(ms: number): Promise<void> {
-    return new Promise((res) => setTimeout(res, ms));
+  private timer(ms: number): Promise<number> {
+    return new Promise((res) => {
+      const timer = setTimeout(() => {
+        res(timer);
+      }, ms);
+    });
   }
 }
 
