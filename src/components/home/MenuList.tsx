e7c8aafd54e3da341374966f52aebfbeb7e1489c
Ferid <feridamir187@gmail.com>
2024-08-01 22:18:11 +0400

    fix: fixes build issues

--- src/components/home/MenuList.tsx
+++ src/components/home/MenuList.tsx
@@ -1,22 +1,16 @@
 import MenuCard from "./MenuCard";
 import testImage from "../../assets/test-image.png";
-// import { A } from "@solidjs/router";
 import Carousel from "../common/Carousel";
 import { For } from "solid-js";
 import { menuList } from "../../types/menu-list";
 
 const MenuList = () => {
-  let ref: HTMLElement;
-
   return (
     <section class="md:w-[70%] w-[90%] m-auto my-6">
       <Carousel title="Featured Visualization" len={5}>
         <For each={menuList}>
           {(item, _) => (
-            <div
-              class="min-w-[180px] md:min-w-[220px] w-[100]"
-              ref={(ele) => (ref = ele)}
-            >
+            <div class="min-w-[180px] md:min-w-[220px] w-[100]">
               <MenuCard
                 title={item.title}
                 image={testImage}
